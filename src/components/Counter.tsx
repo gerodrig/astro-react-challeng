90471de0051fb4f84a5b883542236da48967bc23
gerardo.itpro <gerardo.itpro@gmail.com>
2025-06-17 17:58:40 +0000

    Project initialization

---
+++ src/components/Counter.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './Counter.css';
+
+export default function Counter({
+	children,
+	count: initialCount,
+}: {
+	children: JSX.Element;
+	count: number;
+}) {
+	const [count, setCount] = useState(initialCount);
+	const add = () => setCount((i) => i + 1);
+	const subtract = () => setCount((i) => i - 1);
+
+	return (
+		<>
+			<div className="counter">
+				<button className='hover:cursor-pointer bg-red-500' onClick={subtract}>-</button>
+				<pre>{count}</pre>
+				<button className='hover:cursor-pointer' onClick={add}>+</button>
+			</div>
+			<div className="counter-message">{children}</div>
+		</>
+	);
+}
